4bdd85dece16d7734e3169561297caf73d8a3f95
Thanh Le <lesythanh063@gmail.com>
2021-07-13 15:29:18 +0700

    add template

---
+++ resources/views/page/user/bloglist.blade.php
@@ -0,0 +1,110 @@
+@extends('index')
+@section('content')
+<main id="tg-main" class="tg-main tg-haslayout">
+    <div class="tg-main-section tg-haslayout">
+        <div class="container">
+            <div class="row">
+                <div class="tg-section-name">
+                    <h2>Latest blogs / News</h2>
+                </div>
+                <div class="col-sm-11 col-xs-11 pull-right">
+                    <div class="tg-blogpost tg-blogpostlist">
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-12">
+                                <article class="tg-post">
+                                    <figure class="tg-postimg">
+                                        <a href="#">
+                                            <img src="images/blog/img-14.jpg" alt="image description">
+                                        </a>
+                                        <figcaption>
+                                            <ul class="tg-postmetadata">
+                                                <li><a href="#">by admin</a></li>
+                                                <li><a href="#">04 comments</a></li>
+                                                <li><a href="#">lifestyle</a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="tg-posttitle"><h3><a href="#">dipisicing elit sed do eiusmod tempor indunt</a></h3></div>
+                                    <div class="tg-description">
+                                        <p>Consectetur adipisicing elit sed do eiusmod temport incididunt utia labore et dolore magna aliqua enima ad minim veniam quistrud on ullamco laboris nisiut aliquip exea commodo consequat.</p>
+                                    </div>
+                                    <a class="tg-btn" href="#">Read more</a>
+                                </article>
+                            </div>
+                            <div class="col-sm-6 col-xs-12">
+                                <article class="tg-post">
+                                    <figure class="tg-postimg">
+                                        <a href="#">
+                                            <img src="images/blog/img-15.jpg" alt="image description">
+                                        </a>
+                                        <figcaption>
+                                            <ul class="tg-postmetadata">
+                                                <li><a href="#">by admin</a></li>
+                                                <li><a href="#">04 comments</a></li>
+                                                <li><a href="#">lifestyle</a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="tg-posttitle"><h3><a href="#">dipisicing elit sed do eiusmod tempor indunt</a></h3></div>
+                                    <div class="tg-description">
+                                        <p>Consectetur adipisicing elit sed do eiusmod temport incididunt utia labore et dolore magna aliqua enima ad minim veniam quistrud on ullamco laboris nisiut aliquip exea commodo consequat.</p>
+                                    </div>
+                                    <a class="tg-btn" href="#"><span>read more</span></a>
+                                </article>
+                            </div>
+                            <div class="col-sm-6 col-xs-12">
+                                <article class="tg-post">
+                                    <figure class="tg-postimg">
+                                        <a href="#">
+                                            <img src="images/blog/img-15.jpg" alt="image description">
+                                        </a>
+                                        <figcaption>
+                                            <ul class="tg-postmetadata">
+                                                <li><a href="#">by admin</a></li>
+                                                <li><a href="#">04 comments</a></li>
+                                                <li><a href="#">lifestyle</a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="tg-posttitle"><h3><a href="#">dipisicing elit sed do eiusmod tempor indunt</a></h3></div>
+                                    <div class="tg-description">
+                                        <p>Consectetur adipisicing elit sed do eiusmod temport incididunt utia labore et dolore magna aliqua enima ad minim veniam quistrud on ullamco laboris nisiut aliquip exea commodo consequat.</p>
+                                    </div>
+                                    <a class="tg-btn" href="#">Read more</a>
+                                </article>
+                            </div>
+                            <div class="col-sm-6 col-xs-12">
+                                <article class="tg-post">
+                                    <figure class="tg-postimg">
+                                        <a href="#">
+                                            <img src="images/blog/img-16.jpg" alt="image description">
+                                        </a>
+                                        <figcaption>
+                                            <ul class="tg-postmetadata">
+                                                <li><a href="#">by admin</a></li>
+                                                <li><a href="#">04 comments</a></li>
+                                                <li><a href="#">lifestyle</a></li>
+                                            </ul>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="tg-posttitle"><h3><a href="#">dipisicing elit sed do eiusmod tempor indunt</a></h3></div>
+                                    <div class="tg-description">
+                                        <p>Consectetur adipisicing elit sed do eiusmod temport incididunt utia labore et dolore magna aliqua enima ad minim veniam quistrud on ullamco laboris nisiut aliquip exea commodo consequat.</p>
+                                    </div>
+                                    <a class="tg-btn" href="#"><span>read more</span></a>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="tg-pagination">
+                        <li class="tg-prevpage"><a href="#"><i class="fa fa-angle-left"></i></a></li>
+                        <li class="active"><a href="#">4</a></li>
+                        <li><a href="#">6</a></li>
+                        <li class="tg-nextpage"><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
