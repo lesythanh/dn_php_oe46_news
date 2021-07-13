a5ea05f26ed57cd019738f120db4364fff592ef6
Toan <tin4101999@gmail.com>
2021-07-13 11:30:50 +0700

    Add i18n in init_authentication

---
+++ resources/lang/en/task.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'name' => 'Name',
+    'remember'=> 'Remember Me',
+    'password'=> 'Password',
+    'reset' => 'Reset Password',
+    'send password' => 'Send Password Reset Link',
+    'register' => 'Register',
+    'forgot' => 'Forgot Your Password',
+    'confirm password' => 'Confirm Password',
+    'please confirm your password' => 'Please confirm your password before continuing.',
+    'Verify Your Email Address'=> 'Verify Your Email Address',
+    'A fresh verification link has been sent to your email address'
+    => 'A fresh verification link has been sent to your email address',
+    'Before proceeding, please check your email for a verification link'
+    => 'Before proceeding, please check your email for a verification link',
+    'If you did not receive the email' => 'If you did not receive the email',
+    'click here to request another' => 'click here to request another',
+    'Dashboard' => 'Dashboard',
+    'You are logged in' => 'You are logged in',
+    'Logout' => 'Logout',
+    'E-Mail Address' => 'E-Mail Address',
+    'Home' => 'Home',
+];
---
+++ resources/views/auth/passwords/confirm.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">@lang('task.confirm password')</div>
+
+                <div class="card-body">
+                    @lang('task.please confirm your password')
+                    <form method="POST" action="{{ route('password.confirm') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">@lang('task.password')</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    @lang('task.confirm password')
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        @lang('task.forgot')
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
